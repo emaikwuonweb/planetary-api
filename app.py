39251a305d9cf299805904f779fc14e41b7e3255
Emaikwu Innocent <ennosaint@outlook.com>
2022-08-18 15:18:24 +0100

    Final project file for the planetary-api

--- app.py
+++ app.py
@@ -2,13 +2,27 @@ from flask import Flask, request,jsonify
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import Column, Integer, String, Float
 from flask_marshmallow import Marshmallow
+from flask_jwt_extended import JWTManager,  jwt_required, create_access_token, create_refresh_token
+from flask_mail import Mail, Message
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:admin@localhost:5432/planets'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['JWT_SECRET_KEY'] = "super-secret"
+# Mailtrap config
+app.config['MAIL_SERVER']='smtp.mailtrap.io'
+app.config['MAIL_PORT'] = 2525
+app.config['MAIL_USERNAME'] = '172e339bc6510c'
+app.config['MAIL_PASSWORD'] = '1d6354515d34e7'
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_SSL'] = False
+
 
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
+jwt = JWTManager(app)
+mail = Mail(app)
 
 @app.cli.command('db_create')
 def db_create():
@@ -59,18 +73,112 @@ def db_seed():
     print("Database seeded.")
 
 
-
 # Planets routes
-@app.route('/planets', methods=['GET'])
+@app.route('/planets', methods=['GET', 'POST'])
+@jwt_required(optional=False)
 def planets():
-    planets = Planet.query.all()
-    result = planets_schema.dump(planets)
+    if request.method == 'GET':
+        planets = Planet.query.all()
+        result = planets_schema.dump(planets)
+        return jsonify({
+            "success": True,
+            "planets": result
+        })
+    elif request.method == 'POST':
+        error = False
+        planet = Planet.query.filter(Planet.planet_name==request.get_json()['planet_name']).first()
+        if planet is None:
+            try:
+                new_planet = Planet(
+                    planet_name = request.get_json()['planet_name'],
+                    planet_type = request.get_json()['planet_type'],
+                    home_star = request.get_json()['home_star'],
+                    mass = request.get_json()['mass'],
+                    radius = request.get_json()['radius'],
+                    distance = request.get_json()['distance']
+                )
+                db.session.add(new_planet)
+                db.session.commit()
+            except:
+                db.session.rollback()
+                error = True
+            finally:
+                db.session.close()
+            if error:
+                return jsonify({
+                    "success": False
+                }), 500
+            return jsonify({
+                "success": True,
+            }), 201
+        return jsonify({
+            "success": False,
+            "message": "Planet already exists."
+        }), 409
+
+
+@app.route('/planets/<int:planet_id>', methods=['GET'])
+def show_planet(planet_id):
+    planet = Planet.query.filter(Planet.planet_id==planet_id).first()
+    if planet:
+        return jsonify({
+            "success": True,
+            "planet": planet_schema.dump(planet)
+        })
     return jsonify({
-        "success": True,
-        "planets": result
+        "success": False
+    }), 404
+
+
+@app.route('/planets/<int:planet_id>', methods=['POST'])
+def update_planet(planet_id):
+    planet = Planet.query.filter_by(planet_id=planet_id).first()
+    if planet:
+        error = False
+        try:
+            planet.planet_name = request.get_json()['planet_name']
+            planet.planet_type = request.get_json()['planet_type']
+            planet.home_star = request.get_json()['home_star']
+            planet.mass = request.get_json()['mass']
+            planet.radius = request.get_json()['radius']
+            planet.distance = request.get_json()['distance']    
+            db.session.commit()
+        except:
+            db.session.rollback()
+            error = True
+        finally:
+            db.session.close()
+        if error:
+            return jsonify({
+                "success": False,
+                "message": "An error occured"
+            })
+        return jsonify({
+            "success": True,
+            "planet": planet_schema.dump(planet)
+        }), 202
+    else:
+        return jsonify({
+            "success": False,
+            "message": "Planet does not exist"
+        })
+        
+
+@app.route('/planets/<int:planet_id>', methods=['DELETE'])
+def delete_planet(planet_id):
+    planet = Planet.query.filter_by(planet_id=planet_id)
+    if planet:
+        planet.delete()
+        db.session.commit()
+        return jsonify({
+            "success": True,
+        })
+    return jsonify({
+        "success": False,
+        "message": "Planet not found"
     })
 
-    
+
 @app.route('/register', methods=['POST'])
 def register():
     email = request.form['email']
@@ -91,6 +199,62 @@ def register():
         "user": user_schema.dump(new_user)
     }), 201
 
+
+# JWT
+@app.route("/login", methods=['POST'])
+def login():
+    if request.is_json:
+        email = request.get_json()['email']
+        password = request.get_json()['password']
+    else:
+        email = request.form['email']
+        password = request.form['password']
+
+    user = User.query.filter(User.email==email, User.password==password).first()
+    if user:
+        access_token = create_access_token(identity=email)
+        return jsonify({
+            "success": True,
+            "access_token": access_token
+        })
+    return jsonify({
+        "success": False,
+        "message": "Email or password incorrect."
+    }), 401
+
+
+@app.route('/refresh', methods=['POST'])
+def refresh():
+    if request.is_json:
+        email = request.get_json()['email']
+        password = request.get_json()['password']
+    else:
+        email = request.form['email']
+        password = request.form['password']
+    user = User.query.filter_by(email=email).first()
+    if user:
+        refresh_token = create_refresh_token(identity=email)
+        return jsonify({
+            "success": True,
+            "refresh_token": refresh_token
+        })
+    return jsonify({
+        "success": False,
+        "message": "Email or password incorrect"
+    })
+
+
+@app.route('/retrieve-password/<string:email>', methods=['GET'])
+def retrieve_password(email: String):
+    user = User.query.filter(User.email==email).first()
+    if user:
+        msg = Message(f"Your planetary api password is: {user.password}", sender="admin@planetary-api.com", recipients=['emaikwuoloche@gmail.com'])
+        mail.send(msg)
+        return jsonify({
+            "success": True,
+            "message": f"Password sent to {email}"
+        })
+    return jsonify({"success": False, "message": "Email does not exist."}), 401
 
 
 # Database models
